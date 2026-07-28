8c303c0d7e6b50442f7eb56e20db6cde9bf5e5f4
Kochito-07 <elvistipanta9@gmail.com>
2026-07-27 23:00:18 -0500

    Creacion del codigo de la clase Node y carpeta  models

--- src/models/MapPoint.java
+++ src/models/MapPoint.java
@@ -1,5 +1,34 @@
 package models;
+import java.util.Objects;
 
 public class MapPoint {
+    private String id;
+    private int x;
+    private int y;
+
+    public MapPoint(String id, int x, int y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getId() { return id; }
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MapPoint mapPoint = (MapPoint) o;
+        return Objects.equals(id, mapPoint.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
     
+    @Override
+    public String toString() { return id; }
 }
