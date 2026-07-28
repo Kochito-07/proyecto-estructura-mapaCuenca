ce4f10fbfc28793850ddbd7f3305a3945d83d576
Kochito-07 <elvistipanta9@gmail.com>
2026-07-27 23:55:51 -0500

    Método BFS finalizado

--- src/structures/graphs/Graph.java
+++ src/structures/graphs/Graph.java
@@ -1,5 +1,66 @@
 package structures.graphs;
 
-public class Graph {
-    
+import structures.node.Node;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+public class Graph<T> {
+
+    private final Map<Node<T>, Set<Node<T>>> graph;
+
+    public Graph() {
+        this.graph = new LinkedHashMap<>();
+    }
+    public void add(T data) {
+        Node<T> node = new Node<>(data);
+        graph.putIfAbsent(node, new LinkedHashSet<>());
+    }
+
+    public void addEdge(T v1, T v2) {
+        add(v1);
+        add(v2);
+        Node<T> n1 = new Node<>(v1);
+        Node<T> n2 = new Node<>(v2);
+        graph.get(n1).add(n2);
+        graph.get(n2).add(n1);
+    }
+    public void addEdgeUni(T v1, T v2) {
+        add(v1);
+        add(v2);
+        Node<T> n1 = new Node<>(v1);
+        graph.get(n1).add(new Node<>(v2));
+    }
+    public void remove(T data) {
+        Node<T> target = new Node<>(data);
+        graph.remove(target);
+        for (Set<Node<T>> neighbors : graph.values()) {
+            neighbors.remove(target);
+        }
+    } 
+    public void removeEdge(T v1, T v2) {
+        Node<T> n1 = new Node<>(v1);
+        Node<T> n2 = new Node<>(v2);
+        if (graph.containsKey(n1)) graph.get(n1).remove(n2);
+        if (graph.containsKey(n2)) graph.get(n2).remove(n1);
+    }
+    public void removeEdgeUni(T v1, T v2) {
+        Node<T> n1 = new Node<>(v1);
+        Node<T> n2 = new Node<>(v2);
+        if (graph.containsKey(n1)) graph.get(n1).remove(n2);
+    }
+    public Set<Node<T>> getNodes() {
+        return graph.keySet();
+    }
+    public Map<Node<T>, Set<Node<T>>> getGraph() {
+        return graph;
+    }
+    public Set<Node<T>> getNeighbors(T data) {
+        Node<T> node = new Node<>(data);
+        return graph.getOrDefault(node, new LinkedHashSet<>());
+    }
+    public boolean contains(T data) {
+        return graph.containsKey(new Node<>(data));
+    }
 }
--- src/structures/graphs/implementations/BFSPathFinder.java
+++ src/structures/graphs/implementations/BFSPathFinder.java
@@ -1,5 +1,79 @@
 package structures.graphs.implementations;
 
-public class BFSPathFinder {
-    
+import structures.graphs.Graph;
+import structures.graphs.PathFinder;
+import structures.graphs.PathResult;
+import structures.node.Node;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+public class BFSPathFinder<T> implements PathFinder<T> {
+
+    @Override
+    public PathResult<T> find(Graph<T> graph, T start, T end) {
+        long startTime = System.nanoTime(); 
+
+        Queue<Node<T>> queue = new LinkedList<>();
+        Map<Node<T>, Node<T>> predecessors = new HashMap<>();
+        
+        Set<T> visitados = new LinkedHashSet<>();
+        Set<T> path = new LinkedHashSet<>();
+        
+        Set<Node<T>> visitedNodes = new LinkedHashSet<>();
+
+        Node<T> startNode = new Node<>(start);
+        Node<T> endNode = new Node<>(end);
+
+        queue.add(startNode);
+        visitedNodes.add(startNode);
+
+        boolean found = false;
+
+        while (!queue.isEmpty()) {
+            Node<T> current = queue.poll();
+            visitados.add(current.getData()); 
+
+            if (current.equals(endNode)) {
+                found = true;
+                break;
+            }
+
+            Set<Node<T>> neighbors = graph.getGraph().get(current);
+            if (neighbors != null) {
+                for (Node<T> neighbor : neighbors) {
+                    if (!visitedNodes.contains(neighbor)) {
+                        visitedNodes.add(neighbor);
+                        predecessors.put(neighbor, current);
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+
+        if (found) {
+            List<T> tempPath = new ArrayList<>();
+            Node<T> step = endNode;
+            
+            while (step != null) {
+                tempPath.add(step.getData());
+                step = predecessors.get(step);
+            }
+            
+            Collections.reverse(tempPath);
+            path.addAll(tempPath);
+        }
+
+        long endTime = System.nanoTime();
+        long executionTimeNanos = endTime - startTime;
+
+        return new PathResult<>(visitados, path, found, executionTimeNanos);
+    }
 }
